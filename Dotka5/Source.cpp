470475be97ed49f5ce687f78dbe0e50555ae10c5
SmolskiAnton <kartonanton277@>
2025-05-12 17:49:13 +0300

    new

--- Dotka5/Source.cpp
+++ Dotka5/Source.cpp
@@ -23,6 +23,20 @@ int main()
 		cout << arr[i] << " " ;
 	}
 	cout << endl;
+
+
+	int index_of_max = 0, index_of_min = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > arr[index_of_max])index_of_max = i;
+	}
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > arr[index_of_min])index_of_min = i;
+	}
+	cout << "наибольший элемент равен" << arr[index_of_max] << "и находится на" << index_of_min + 1 << "позиции" << endl;
+	cout << "наименьший элемент равен" << arr[index_of_min] << "и находится на" << index_of_min + 1 << "позиции" << endl;
+
 	delete[]arr;
 
 }
