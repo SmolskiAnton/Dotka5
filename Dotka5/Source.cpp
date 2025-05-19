c216e4c30f4b6c7f67e16b1faf1d5496f147fb75
SmolskiAnton <kartonanton277@>
2025-05-19 19:03:11 +0300

    newlуе

--- Dotka5/Source.cpp
+++ Dotka5/Source.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int main()
 {
+	//создание и заполнение динамического массива
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
 	int a, b, n;
@@ -24,7 +25,7 @@ int main()
 	}
 	cout << endl;
 
-
+	//поиск макс. и минимального элемента массива
 	int index_of_max = 0, index_of_min = 0;
 	for (int i = 1; i < n; i++)
 	{
@@ -39,20 +40,51 @@ int main()
 
 
 
-
+	//сортировка по убыванию
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
 			if (arr[j] < arr[i]) swap(arr[i], arr[j]);
 		}
 	}
 
+	//сортировка по возрастанию
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
 			if (arr[j] > arr[i]) swap(arr[i], arr[j]);
 		}
 	}
+	//уменьшения размера массива
+	int index;
+	cin >> index;
+	for (int i = index; i < n - 1; i++) {
+		
+			arr[i] = arr[i+1];
+		
+	}
+	n--;
+	//увеличение размера массива
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	int m = n + 1;
+	int* new_arr = new int[m];
+	for (int i = 0; i < n; i++) {
+
+		new_arr[i] = arr[i];
+
+	}
+
+	new_arr[m - 1] = 100;
+	delete[]arr;
+
+	arr = new_arr;
+	n = m;
 
 
+//завершения создания массива
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << " ";
